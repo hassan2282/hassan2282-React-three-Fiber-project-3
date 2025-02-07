480b1802beabcba7a16ee9eabdd3125eae5f5415
سید حسن تقوی <taghavey.hassan@gmail.com>
2025-02-07 22:29:01 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Icon } from 'react-icons-kit'
+import { home } from 'react-icons-kit/icomoon/home'
+
+function App() {
+  return (
+    <div>
+      <Icon size={32} icon={home} className='p-2 text-red-400'/>
+    </div>
+  );
+}
+
+export default App;
